d4f6a6721536285a181c9853b5b87d49e0bdd92c
Camila-Torres-Ramos <82998251+Camila-Torres-Ramos@users.noreply.github.com>
2022-04-26 14:00:49 -0500

    Add files via upload

---
+++ Lab2-Ejercicio 3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(){
+	cout<<"Multiplos del 5 al 100"<<endl;
+	for(int i=1;i<=100;i++)
+	{
+		if (i%5==0) cout<<i<<endl;
+	}	
+	
+	return 0;
+}
